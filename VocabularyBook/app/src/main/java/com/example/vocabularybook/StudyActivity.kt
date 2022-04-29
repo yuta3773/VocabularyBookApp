5e5435f3b2f3e5270a6528bd80ae6e0436963698
神塚佑太 <cybor_ggg@icloud.com>
2022-04-29 12:11:09 +0900

    StudyActivityビュー作成

---
+++ VocabularyBook/app/src/main/java/com/example/vocabularybook/StudyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vocabularybook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_study)
+    }
+}
